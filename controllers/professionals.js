94960bef4f6b397dc79ff93b857a8e7d5fd540ef
Eduardo Prieto <eduardo.prieto@nexgen-soluciones.com>
2022-09-19 13:18:05 -0500

    Team Lesson 2 Completed - w stretch challenge

---
+++ controllers/professionals.js
@@ -0,0 +1,11 @@
+const professionalSchema = require("../models/professionals");
+
+const getProfessional = async (req, res) => {
+    await professionalSchema.find()
+        .then((data) => res.json(data[0]))
+        .catch((err) => res.json({error : err}));
+}
+
+module.exports = {
+    getProfessional
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+const professionalRouter = require("./professionals")
 
 router.get("/", (req, res) => {
     console.log("Health check");
     res.status(200).send("Health check");
 });
+
+router.use("/professional", professionalRouter);
 
 module.exports = router;
